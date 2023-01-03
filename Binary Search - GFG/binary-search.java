f8d33c61181fc0dd533ab061aa49be6d38da27f8
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2023-01-03 23:03:07 +0530

    Added solution - LeetHub

---
+++ Binary Search - GFG/binary-search.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+  public class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int key = sc.nextInt();
+            Solution g = new Solution();
+            System.out.println(g.binarysearch(arr, n, key));
+            T--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int start = 0;
+        int end = n - 1;
+        // line below is used whether it's order agnostic binary tree or not.
+        boolean isAsc = arr[start] < arr[end];
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == k) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (k < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (k > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
