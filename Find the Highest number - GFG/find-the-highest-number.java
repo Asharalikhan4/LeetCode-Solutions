5f43f9082a0f23cf36153b7b5afb8fdc07052ddc
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2023-01-03 22:25:29 +0530

    Added solution - LeetHub

---
+++ Find the Highest number - GFG/find-the-highest-number.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String s = br.readLine().trim();
+            int n = Integer.parseInt(s);
+            String S = br.readLine();
+            String[] s1 = S.split(" ");
+            List<Integer> a = new ArrayList<Integer>(); 
+            for( int i = 0; i < n; i++)
+            {
+                a.add(Integer.parseInt(s1[i]));
+            }
+            Solution ob = new Solution();
+            int ans = ob.findPeakElement(a);
+            System.out.println(ans);
+
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int findPeakElement(List<Integer> a)
+    {
+        // Code here
+        int low = 0, high = a.size() - 1, mid;
+        while(low < high){
+            mid = low+(high - low)/2;
+            if(a.get(mid) > a.get(mid+1)){
+                high = mid;
+            }else {
+                low = mid + 1;
+            }
+        }
+        return a.get(low);
+    }
+}
