f554ae1dbdcb4f61cbc91f950b853b4e0e3e7168
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2023-01-05 21:55:28 +0530

    Time: 2 ms (75.05%), Space: 54.6 MB (51.15%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+    // Code here
+    if (nums == null || nums.length == 0) return;        
+
+    int insertPos = 0;
+    for (int num: nums) {
+        if (num != 0) nums[insertPos++] = num;
+    }        
+
+    while (insertPos < nums.length) {
+        nums[insertPos++] = 0;
+    }
+    }
+}
