ecc22b4092d12a81138b75353ae0dcfd9d49a354
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2023-01-06 23:27:56 +0530

    Added solution - LeetHub

---
+++ Search insert position of K in a sorted array - GFG/search-insert-position-of-k-in-a-sorted-array.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int Arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                Arr[i] = Integer.parseInt(input_line[i]);
+            int k = Integer.parseInt(read.readLine());
+            
+            Solution ob = new Solution();
+            System.out.println(ob.searchInsertK(Arr, N, k));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int searchInsertK(int Arr[], int N, int k)
+    {
+        // code here
+        int start = 0, end = Arr.length - 1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(k < Arr[mid]) end = mid - 1;
+            else if(k > Arr[mid]) start = mid + 1;
+            else return mid;
+        }
+        return start;
+    }
+}
