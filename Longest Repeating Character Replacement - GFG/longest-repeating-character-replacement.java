0adc49b5f837ac342fa9f0a9554cfa307fa611bb
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2023-01-28 22:14:24 +0530

    Added solution - LeetHub

---
+++ Longest Repeating Character Replacement - GFG/longest-repeating-character-replacement.java
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        FastReader sc = new FastReader(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            String s = sc.next();
+            int k = sc.nextInt();
+            Solution obj = new Solution();
+            s = s.toUpperCase();
+            int ans = obj.characterReplacement(s, k);
+            System.out.println(ans);
+        }
+    }
+    static class FastReader {
+
+        byte[] buf = new byte[2048];
+        int index, total;
+        InputStream in;
+
+        FastReader(InputStream is) { in = is; }
+
+        int scan() throws IOException {
+            if (index >= total) {
+                index = 0;
+                total = in.read(buf);
+                if (total <= 0) {
+                    return -1;
+                }
+            }
+            return buf[index++];
+        }
+
+        String next() throws IOException {
+            int c;
+            for (c = scan(); c <= 32; c = scan())
+                ;
+            StringBuilder sb = new StringBuilder();
+            for (; c > 32; c = scan()) {
+                sb.append((char)c);
+            }
+            return sb.toString();
+        }
+
+        int nextInt() throws IOException {
+            int c, val = 0;
+            for (c = scan(); c <= 32; c = scan())
+                ;
+            boolean neg = c == '-';
+            if (c == '-' || c == '+') {
+                c = scan();
+            }
+            for (; c >= '0' && c <= '9'; c = scan()) {
+                val = (val << 3) + (val << 1) + (c & 15);
+            }
+            return neg ? -val : val;
+        }
+
+        long nextLong() throws IOException {
+            int c;
+            long val = 0;
+            for (c = scan(); c <= 32; c = scan())
+                ;
+            boolean neg = c == '-';
+            if (c == '-' || c == '+') {
+                c = scan();
+            }
+            for (; c >= '0' && c <= '9'; c = scan()) {
+                val = (val << 3) + (val << 1) + (c & 15);
+            }
+            return neg ? -val : val;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int characterReplacement(String S, int K) {
+        // code here
+        int len = S.length();
+        int[] count = new int[26];
+        int start = 0, maxCount = 0, maxLength = 0;
+        for (int end = 0; end < len; end++) {
+            maxCount = Math.max(maxCount, ++count[S.charAt(end) - 'A']);
+            while (end - start + 1 - maxCount > K) {
+                count[S.charAt(start) - 'A']--;
+                start++;
+            }
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+        return maxLength;
+    }
+}
