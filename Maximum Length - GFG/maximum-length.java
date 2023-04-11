6fb2bd1d40e46ff61b76b259f149393161592ef7
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2023-04-11 23:40:15 +0530

    Added solution - LeetHub

---
+++ Maximum Length - GFG/maximum-length.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class FastReader{ 
+    BufferedReader br; 
+    StringTokenizer st; 
+
+    public FastReader(){ 
+        br = new BufferedReader(new InputStreamReader(System.in)); 
+    } 
+
+    String next(){ 
+        while (st == null || !st.hasMoreElements()){ 
+            try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    String nextLine(){ 
+        String str = ""; 
+        try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+        return str; 
+    }
+    
+    Integer nextInt(){
+        return Integer.parseInt(next());
+    }
+} 
+    
+class GFG {
+    public static void main(String args[]) throws IOException {
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+    
+            Solution ob = new Solution();
+            int ans = ob.solve(a, b, c);
+            
+            out.println(ans);
+        }
+        out.flush();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int solve(int a, int b, int c) {
+        // code here
+        if(a==b && b==c) return a*b;
+       
+       int x=(c+b+1)*2;
+       if(a>x) return -1;
+       int y=(a+c+1)*2;
+       if(b>y) return -1;
+       int z=(a+b+1)*2;
+       if(c>z) return -1;
+       
+       return a+b+c;
+    }
+};
