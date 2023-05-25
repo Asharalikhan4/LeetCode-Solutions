0ed2ab1660d216bad2f862723fcd97808dc27455
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2023-05-25 20:32:26 +0530

    Added solution - LeetHub

--- Expression Add Operators - GFG/expression-add-operators.java
+++ Expression Add Operators - GFG/expression-add-operators.java
@@ -29,29 +29,38 @@ class GFG {
 
 
 class Solution {
-    public static void helper(ArrayList<String> ans, String res, String num, int target, int index, long val, long mul) {
-        if(index == num.length()) {
-            if(val == target) 
-                ans.add(res);
+    public static ArrayList<String> addOperators(String S, int target)
+    {
+        ArrayList<String> result = new ArrayList<>();
+        genExpression(S, target, 0, 0, 0,"",result);
+        return result;
+    }
+    
+    private static void genExpression(String num, int target, int index, long value, long prevOp, String exp, ArrayList<String> result){
+        if (index == num.length())
+        {
+            if (value == target)
+            {
+                result.add(exp);
+            }
             return;
         }
-        for(int i =index; i < num.length(); i++) {
-            if(i != index && num.charAt(index) == '0') break;
-            long cur = Long.parseLong(num.substring(index, i + 1));
-            if(index == 0) {
-                helper(ans, res + cur, num, target, i + 1, cur, cur);
-            } else {
-                helper(ans, res + "+" + cur, num, target, i + 1, val + cur, cur);
-                helper(ans, res + "-" + cur, num, target, i + 1, val - cur, -cur);
-                helper(ans, res + "*" + cur, num, target, i + 1, val - mul + mul * cur, mul * cur);
-            }
+        long currOp = 0;
+        for (int i = index; i < num.length(); i++)
+        {
+            if (i != index && num.charAt(index) == '0')
+                break;
+                
+            currOp = currOp* 10 +(num.charAt(i) - '0');
+            
+            if(index == 0)
+                genExpression(num, target, i+1, currOp,currOp, Long.toString(currOp), result);
+            else
+            {
+                genExpression(num, target, i+1, value + currOp, currOp, exp + '+' + currOp, result);
+                genExpression(num, target, i+1, value - currOp, -currOp, exp + '-' + currOp, result);
+                genExpression(num, target, i+1, value - prevOp + prevOp * currOp, prevOp * currOp, exp + '*' + currOp, result);
+            }    
         }
-    }
-    public static ArrayList<String> addOperators(String S, int target) {
-        // code here
-        ArrayList<String> ans = new ArrayList<>();
-        if(S.length() == 0 || S == null) return ans;
-        helper(ans, "", S, target, 0, 0, 0);
-        return ans;
     }
 }
