97c2eb711066f4d99991401415cbbdc362946f68
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2023-06-13 13:31:40 +0530

    Time: 108 ms (18.06%), Space: 53.1 MB (9.35%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int equalPairs(int[][] a) {
+        HashMap<String,Integer> h=new HashMap<>();
+        int n=a.length;
+        String x="";
+        for(int i=0;i<n;i++){
+            x="";
+            for(int j=0;j<n;j++){
+                x+=a[i][j]+"+";
+            }
+            h.put(x,h.getOrDefault(x,0)+1);
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            x="";
+            for(int j=0;j<n;j++){
+                x+=a[j][i]+"+";
+            }
+            if(h.containsKey(x)){
+                count+=h.get(x);
+            } 
+        }
+        return count;
+    }
+}
