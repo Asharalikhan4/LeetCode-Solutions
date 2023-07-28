c647607a954626d7a9036f18c337c7f98010914d
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2023-07-28 10:42:49 +0530

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.java
+++ Reverse a String - GFG/reverse-a-string.java
@@ -1,49 +1,31 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial template for Java
 
 import java.util.*;
 import java.io.*;
-import java.lang.*;
-
-class Driver
-{
-    public static void main(String args[])throws IOException
-    {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-        
-        while(t-- >0)
-        {
-            String str = read.readLine();
-            System.out.println(new Reverse().reverseWord(str));
+        while (t-- > 0) {
+            String S = read.readLine();
+            Solution ob = new Solution();
+
+            System.out.println(ob.revStr(S));
         }
     }
 }
 // } Driver Code Ends
 
 
-//User function Template for Java
-
-
-class Reverse
-{
-    // Complete the function
-    // str: input string
-    public static String reverseWord(String str)
-    {
+class Solution {
+    static String revStr(String S) {
         // code here
-        char[] charArray = str.toCharArray();
-        int left = 0;
-        int right = charArray.length - 1;
-
-        while (left < right) {
-            char temp = charArray[left];
-            charArray[left] = charArray[right];
-            charArray[right] = temp;
-            left++;
-            right--;
+        String ans = "";
+        for(int i = S.length()-1; i >= 0; i--){
+            ans = ans + S.charAt(i);
         }
-
-        return new String(charArray);
+        return ans;
     }
 }
