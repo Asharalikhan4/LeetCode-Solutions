f12e4b750f37f0f6f739773e76a879979442e9fe
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2023-08-10 01:44:34 +0530

    Added solution - LeetHub

--- Minimum element in BST - GFG/minimum-element-in-bst.java
+++ Minimum element in BST - GFG/minimum-element-in-bst.java
@@ -121,6 +121,10 @@ class Tree {
     int minValue(Node node) {
         if(node == null) return -1;
         if(node.left == null) return node.data;
-        return minValue(node.left);
+        Node temp = node;
+        while(temp.left != null){
+            temp = temp.left;
+        }
+        return temp.data;
     }
 }
