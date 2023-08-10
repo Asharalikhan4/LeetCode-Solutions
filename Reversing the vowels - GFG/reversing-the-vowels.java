606339db6b670e0d8e56bb226b9cc5a83caaf666
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2023-08-10 17:09:49 +0530

    Added solution - LeetHub

---
+++ Reversing the vowels - GFG/reversing-the-vowels.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            String s;
+            s = sc.next();
+           
+            Solution ob = new Solution();
+            
+            System.out.println(ob.modify(s));    
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    String modify (String s)
+    {
+        // your code here
+        StringBuilder ans = new StringBuilder();
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u'){
+                st.push(s.charAt(i));
+            }
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u'){
+                ans.append(st.pop());
+            } else {
+                ans.append(s.charAt(i));
+            }
+        }
+        return ans.toString();
+    }
+}
