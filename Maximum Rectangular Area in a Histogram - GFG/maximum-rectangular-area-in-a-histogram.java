d260fb4d1be2bc3f3f5ea14bd45830a5b1140a0f
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2023-08-11 22:49:13 +0530

    Added solution - LeetHub

---
+++ Maximum Rectangular Area in a Histogram - GFG/maximum-rectangular-area-in-a-histogram.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    long n = Long.parseLong(br.readLine().trim());
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    long[] arr = new long[(int)n];
+		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
+		    System.out.println(new Solution().getMaxArea(arr, n));
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find largest rectangular area possible in a given histogram.
+    public static long getMaxArea(long hist[], long n) 
+    {
+        // your code here
+        Stack<Integer> stack = new Stack<>();
+        long maxArea = 0;
+        for (int i = 0; i <= n; i++) {
+            long currentHeight = (i == n) ? 0 : hist[i];
+
+            while (!stack.isEmpty() && currentHeight <= hist[stack.peek()]) {
+                long height = hist[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+        return maxArea;
+    }
+        
+}
+
+
+
