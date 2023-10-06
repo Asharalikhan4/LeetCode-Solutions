9311c7cc7c1cd114e848ce4b4b6929cbc8dbe1bf
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2023-10-06 23:22:23 +0530

    Added solution - LeetHub

---
+++ Reverse alternate nodes in Link List - GFG/reverse-alternate-nodes-in-link-list.java
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Rearr
+{
+    static Node head;
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t  =sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node temp = head;
+            for(int i = 1; i < n; i++)
+            {
+                int a = sc.nextInt();
+                temp.next = new Node(a);
+                temp = temp.next;
+            }
+            
+            Solution ob = new Solution();
+            ob.rearrange(head);
+            printLast(head);
+            System.out.println();
+        }
+    }
+    
+    public static void printLast(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*node class of the linked list
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}*/
+class Solution
+{
+    public static void rearrange(Node odd)
+    {
+        // add your code here
+        if(odd.next==null) return;
+        Node temp=odd,temp1=odd.next,prev=odd;
+        temp.next=temp.next.next;
+        temp1.next=null;
+        temp=temp.next;
+        while(temp!=null && temp.next!=null){
+            Node temp2=temp.next.next;
+            temp.next.next=temp1;
+            temp1=temp.next;
+            temp.next=temp2;
+            prev=temp;
+            temp=temp.next;
+        }
+        if(temp!=null){
+            temp.next=temp1;
+        }
+        else{
+            prev.next=temp1;
+        }
+    }
+}
