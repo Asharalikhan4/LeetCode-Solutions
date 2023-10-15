3941f2a19f1d41789cf0673c22bf8782e9f7a0f5
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2023-10-15 17:17:34 +0530

    Added solution - LeetHub

---
+++ Second Largest - GFG/second-largest.js
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function printList(res,n){
+    let s="";
+    for(let i=0;i<n;i++){
+        s+=res[i];
+        s+=" ";
+    }
+    console.log(s);
+}
+
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let n = parseInt(readLine());
+        let arr = new Array(n);
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n;i++){
+            arr[i] = input_ar1[i];
+        }
+        let obj = new Solution();
+        let res = obj.print2largest(arr, n);
+        console.log(res);
+        
+    }
+}// } Driver Code Ends
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+
+/**
+ * @param {number[]} arr
+ * @param {number} n
+ * @returns {number}
+*/
+
+class Solution{
+    print2largest(arr,n){
+        //code here
+        let largest = -1, secondLargest = 0;
+        for(let num of arr){
+            if(num > largest){
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num < largest){
+                secondLargest = num;
+            }
+        }
+        return secondLargest;
+    }
+}
+
