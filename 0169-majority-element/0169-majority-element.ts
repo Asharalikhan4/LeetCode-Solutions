b58d99cf7b0e96790fa232b724f1dca7add15859
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2024-02-23 10:29:50 +0530

    Time: 77 ms (14.16%), Space: 53.9 MB (22.45%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.ts
@@ -0,0 +1,28 @@
+function majorityElement(arr: number[]): number {
+    let n: number = arr.length;
+    let cnt: number = 0;
+    let el: number;
+
+    for (let i = 0; i < n; i++) {
+        if (cnt === 0) {
+            cnt = 1;
+            el = arr[i];
+        } else if (el === arr[i]) {
+            cnt++;
+        } else {
+            cnt--;
+        }
+    }
+
+    let cnt1 = 0;
+    for (let i = 0; i < n; i++) {
+        if (arr[i] === el) {
+            cnt1++;
+        }
+    }
+
+    if (cnt1 > Math.floor(n / 2)) {
+        return el;
+    }
+    return -1;
+};
